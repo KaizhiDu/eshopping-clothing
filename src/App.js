a490f78fce419b7873d7ccaa2860dd2573c113a7
kaizhi du <kaizhidu@kaizhis-mbp.home>
2019-12-19 19:35:28 -0800

    add firebase include google oauth and sign up

--- src/App.js
+++ src/App.js
@@ -6,16 +6,54 @@ import { Route, Switch } from 'react-router-dom';
 
 import './App.css';
 import Header from "./components/header/header.component";
+import SignInUp from "./pages/sign-in-up/sign-in-up.component";
+import { auth, createUserProfileDocument } from './firebase/firebase-utils-config';
 
 
 class App extends React.Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            currentUser: null
+        };
+    }
+
+    unsubscribeFromAuth = null;
+
+    componentDidMount() {
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+            if (userAuth) {
+                const userRef = await createUserProfileDocument(userAuth);
+                userRef.onSnapshot(snapshot => {
+                    this.setState({
+                        currentUser: {
+                            id: snapshot.id,
+                            ...snapshot.data()
+                        }
+                    }, () => {
+                        console.log(this.state);
+                    });
+                });
+            } else {
+                this.setState({currentUser: userAuth});
+            }
+        });
+    }
+
+    componentWillUnmount() {
+        this.unsubscribeFromAuth();
+    }
+
     render() {
         return (
             <div className='App'>
-                <Header/>
+                <Header currentUser={this.state.currentUser}/>
                 <Switch>
                     <Route exact path='/' component={HomePage}/>
                     <Route exact path='/shop' component={ShopPage}/>
+                    <Route exact path='/signin' component={SignInUp}/>
                 </Switch>
             </div>
         );
